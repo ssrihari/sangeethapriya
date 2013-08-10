31b3f8fffe4c876ca660ec9fd9cab414681bcd55
Srihari Sriraman <srihari@c42.in>
2013-08-10 13:04:30 +0530

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sangeethapriya::Application.config.secret_token = 'ba643b3b9ddc362911ca07b9e19b832faeb69cfa2828aec622542e0b75dd51390eb3bce3b679a2a6c94a779e2f7e508359a3cf6a7f12496f858b1cfa53ea7f70'
