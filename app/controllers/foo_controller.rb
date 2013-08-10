3f5f850733033ea37381c650a8241de90f577527
Srihari Sriraman <srihari@c42.in>
2013-08-10 16:18:49 +0530

    Autocomplete search by raga

--- app/controllers/foo_controller.rb
+++ app/controllers/foo_controller.rb
@@ -3,5 +3,6 @@ class FooController < ApplicationController
     raga = params[:raga] || 'kalyani'
     searcher = SangeethapriyaSearcher.new("http://www.sangeethapriya.org/fs_search.php", raga)
     @results = searcher.results
+    @ragas = YAML.load_file('config/raga.yml')
   end
 end
