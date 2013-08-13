8d9e5dc983dcfb06c08e258909ddfa6aa0db9392
Srihari Sriraman <srihari@c42.in>
2013-08-13 09:14:30 +0530

    Removing sangeethamshare

--- app/controllers/foo_controller.rb
+++ app/controllers/foo_controller.rb
@@ -2,7 +2,7 @@ class FooController < ApplicationController
   def search
     params[:query] = 'tmk' unless params[:query].present?
     searcher = SangeethapriyaSearcher.new("http://www.sangeethapriya.org/fs_search.php", params[:query])
-    @results = searcher.results
+    @results = searcher.only_sangeethapriya_results
     @ragas = YAML.load_file('config/raga.yml')
   end
 end
